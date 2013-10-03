f587e62a2ea737435e06452f8e6f8902486a3546
Mike Schwörer <mailport@mikescher.de>
2013-10-03 16:44:59 +0200

    Fixed bug when the SSL certificate is outdated / invalid (F*** Rapla) [3rd try ... SmartGit fail ?]

--- RaplaPreviewWidget/src/main/java/org/semtex/HTMLRequestTask.java
+++ RaplaPreviewWidget/src/main/java/org/semtex/HTMLRequestTask.java
@@ -8,13 +8,29 @@ import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.X509HostnameVerifier;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.SingleClientConnManager;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 /**
  * Created by ZEUS on 30.08.13.
@@ -33,7 +49,8 @@ public class HTMLRequestTask extends AsyncTask<String, String, String> {
         HttpConnectionParams.setConnectionTimeout(httpParameters, 5000); // 5s max for connection
         HttpConnectionParams.setSoTimeout(httpParameters, 6000); // 6s max to get data
 
-        HttpClient httpclient = new DefaultHttpClient();
+        HttpClient httpclient = MySSLSocketFactory.getNewHttpClient();
+
         HttpResponse response;
         String responseString = null;
         try {
